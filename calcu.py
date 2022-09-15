a27c668ca7b31d6baa00a95188b5c02e933a748f
julia <juregenes@gmail.com>
2022-09-15 12:28:32 -0300

    calculadora

---
+++ calcu.py
@@ -0,0 +1,38 @@
+print("calculadora")
+
+sair = False
+
+while sair == False:
+
+    valor1 = input("Digite o primeiro numero: ")
+    valor1 = int(valor1)
+    operador = input("Digite o operador (+-/*): ")
+    valor2 = input("Digite o segundo numero: ")
+    valor2 = int(valor2)
+
+    #soma   
+    if operador == "+":
+        operacao = valor1 + valor2
+
+    #subtracao       
+    if operador == "-":
+        operacao = valor1 - valor2
+
+    #divisao           
+    if operador == "/":
+        operacao = valor1 / valor2
+
+     #multiplicacao           
+    if operador == "*":
+        operacao = valor1 * valor2
+
+    print("resultado: ")
+    print(operacao)
+
+    teste = input("Deseja sair (n/s): ")
+    if teste == "s":
+        sair = True
+        
+         
+    
+     
